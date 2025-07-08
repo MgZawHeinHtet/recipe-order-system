877c1105dca336d64eb39e188cb4790367e7c541
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2025-07-08 21:22:17 +0630

    fix : import on appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+           if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
