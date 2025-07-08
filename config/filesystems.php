32fa0d89881ccd4cc08558f38fe4b6873bbaaf8b
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2025-07-08 21:44:09 +0630

    fix image

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,7 +42,7 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
-        ],
+        ],  
 
         's3' => [
             'driver' => 's3',
